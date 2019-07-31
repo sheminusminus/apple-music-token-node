4143032c865cb7d9871551aa3351146953888c53
Emily Kolar <emkolar@gmail.com>
2019-07-31 02:16:51 -0500

    split into sync and async api

--- index.js
+++ index.js
@@ -4,30 +4,8 @@ const moment = require('moment');
 
 const InvalidCertPathError = require('./invalidCertPathError');
 
-/**
- * @function getToken - Generates a developer token for use with the Apple Music API
- * @param {string} certPath - Absolute path to your .p8 private key file
- * @param {string} teamId - Developer team id
- * @param {string} keyId - Music Key id
- * @param {string} hoursValid - Hours until the key should expire
- * @returns {{token: string, expiresAt: timestamp, expiresIn: seconds}} -
- * ES256 encoded JWT token, time of expiration (in seconds), and also the seconds
- * remaining until expiration occurs.
- *
- * (`expiresIn` is sort of a lazy solution on my part, but is included to avoid
- * requiring anyone to deal with side-effects from date conversions between server
- * and client code.)
- */
-const getToken = (certPath, teamId, keyId, hoursValid = 12) => {
-  if (!fs.lstatSync(certPath).isFile()) {
-    throw new InvalidCertPathError(
-      { certPath },
-      'Cert path provided to apple-music-token-node is not a valid file path',
-    );
-  }
-
+const getResult = (cert, teamId, keyId, hoursValid) => {
   const alg = 'ES256';
-  const cert = fs.readFileSync(certPath);
   const now = moment().unix();
   const expiration = moment().add(hoursValid, 'hours').unix();
 
@@ -56,4 +34,73 @@ const getToken = (certPath, teamId, keyId, hoursValid = 12) => {
   };
 };
 
-module.exports = getToken;
+/**
+ * @function getToken - Generates a developer token for use with the Apple Music API
+ * @param {string} certPath - Absolute path to your .p8 private key file
+ * @param {string} teamId - Developer team id
+ * @param {string} keyId - Music Key id
+ * @param {string} hoursValid - Hours until the key should expire
+ * @returns {{token: string, expiresAt: timestamp, expiresIn: seconds}} -
+ * ES256 encoded JWT token, time of expiration (in seconds), and also the seconds
+ * remaining until expiration occurs.
+ *
+ * (`expiresIn` is sort of a lazy solution on my part, but is included to avoid
+ * requiring anyone to deal with side-effects from date conversions between server
+ * and client code.)
+ */
+const getToken = (certPath, teamId, keyId, hoursValid = 12) => {
+  try {
+    if (!fs.lstatSync(certPath).isFile()) {
+      throw new InvalidCertPathError(
+        { certPath },
+        'Cert path provided to apple-music-token-node is not a valid file path',
+      );
+    }
+  } catch (err) {
+    throw err;
+  }
+
+  const cert = fs.readFileSync(certPath);
+
+  return getResult(cert, teamId, keyId, hoursValid);
+};
+
+/**
+ * @function getTokenAsync - (Async) Generates a developer token for use with the Apple Music API
+ * @param {string} certPath - Absolute path to your .p8 private key file
+ * @param {string} teamId - Developer team id
+ * @param {string} keyId - Music Key id
+ * @param {string} hoursValid - Hours until the key should expire
+ * @returns {Promise} -
+ * Resolves to `{token: string, expiresAt: timestamp, expiresIn: seconds}` (ES256 encoded JWT token,
+ * time of expiration (in seconds), and also the seconds remaining until expiration occurs).
+ */
+const getTokenAsync = (certPath, teamId, keyId, hoursValid = 12) => {
+  return new Promise((resolve, reject) => {
+    fs.lstat(certPath, (err, stats) => {
+      if (err) {
+        return reject(err);
+      }
+
+      if (!stats.isFile()) {
+        return reject(new InvalidCertPathError(
+          { certPath },
+          'Cert path provided to apple-music-token-node is not a valid file path',
+        ));
+      }
+
+      fs.readFile(certPath, (err, cert) => {
+        if (err) {
+          return reject(err);
+        }
+
+        const result = getResult(cert, teamId, keyId, hoursValid);
+
+        return resolve(result);
+      });
+    });
+  });
+};
+
+module.exports.getToken = getToken;
+module.exports.getTokenAsync = getTokenAsync;
